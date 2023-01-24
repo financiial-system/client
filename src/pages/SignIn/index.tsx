effb27361cb1aaa642f6913f20ff1cf1f01988df
Daniel Josias <daniieljosiaslima@gmail.com>
2023-01-24 20:11:40 -0300

    feat: página signin

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import SignIn from "../../components/Signin"
+
+export default function index() {
+  return (
+    <div>
+      <SignIn/>
+    </div>
+  )
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
-import SignUp from '../components/SignUp'
 import Welcome  from '../components/Welcome'
+import SignUp from '../components/SignUp'
+import SignIn from '../components/Signin'
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Welcome/>} />
       <Route path="/signup" element={<SignUp/>} />
+      <Route path="/signin" element={<SignIn/>} />
     </Routes>
   )
 }
