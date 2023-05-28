36cf4fcd0dc71536ae560487692d99e84c1d51f5
Daniel Josias <daniieljosiaslima@gmail.com>
2023-05-28 18:56:24 -0300

    feat: providers criado com sucesso

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { GlobalStyle } from './styles/global'
 import { BrowserRouter } from 'react-router-dom'
+import { UserProvider } from './providers/userContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle/>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UserProvider>
+      <GlobalStyle/>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+    </UserProvider>
   </React.StrictMode>,
 )
