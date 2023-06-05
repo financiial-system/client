6e6af477c234b21ec7271059b4a8539bf44a5ec3
Daniel Josias <daniieljosiaslima@gmail.com>
2023-06-05 09:24:38 -0300

    feat: nome e avatar dinâmicos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,11 +3,19 @@ import { useMediaQuery } from 'usehooks-ts'
 import { Header, Name, Logotipo, Container, Avatar, Text, Logout } from "./styles"
 import { IoMdLogOut } from 'react-icons/io'
 import avatar from '../../assets/avatar.jpg'
+import { useContext } from 'react'
+import { UserContext } from '../../providers/userContext'
+import { TransactionsContext } from '../../providers/transactionsContext'
 
 export default function header() {
     const history = useHistory()
     const isMobile = useMediaQuery("(min-width: 375px)")
     const isTablet = useMediaQuery("(min-width: 768px)")
+
+    const { transactions } = useContext(TransactionsContext)
+    const userId = localStorage.getItem("userId")
+    const datas = transactions.filter((transactions:any) => transactions.user?.id === userId)
+    const [data] = datas
 
     const clearToken = () => {
       localStorage.removeItem("token")
@@ -16,10 +24,10 @@ export default function header() {
 
   return (
     <Header>
-        <Name>Danie J.</Name>
+        <Name>{data?.user.name}</Name>
         <Logotipo props={{isMobile, isTablet}}>Sistema Financeiro</Logotipo>
         <Container>
-            <Avatar src={avatar} alt="avatar do usuário"></Avatar>
+            <Avatar src={data?.user.avatar} alt="avatar do usuário"></Avatar>
             <Text>Sair</Text>
             <Logout onClick={() => clearToken()}>
                 <IoMdLogOut fontSize={25}></IoMdLogOut>
